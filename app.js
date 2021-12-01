53aed9d6e02377d9abc2c9d7ff9fd658e4a27339
Liam Eller <liamthelastson@gmail.com>
2021-12-01 09:15:50 -0800

    Add my own work to new branch

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 import Phaser from 'phaser'
 
 const config = {
-  width: 800,
-  height: 600,
-  backgroundColor: 0xffffff,
+  width: 1000,
+  height: 800,
+  backgroundColor: 0x333333,
   scene: {
     preload,
     create,
@@ -22,36 +22,20 @@ const game = new Phaser.Game(config);
 
 function preload() {
   this.load.image('ground', 'assets/img/ground.png');
-  this.load.image('island', 'assets/img/island.png');
-  this.load.image('star', 'assets/img/star.png');
-  this.load.spritesheet('player', 'assets/img/player.png', {
-    frameWidth: 32,
-    frameHeight: 48
-  })
+  this.load.spritesheet('player', 'assets/img/ship.png', 'assets/json/spritesPositions.json')
 }
+// this.player.scale.setTo(-2, -1);
 
 function create() {
 
   let platforms = this.physics.add.staticGroup();
   platforms.create(400, 588, "ground");
-  platforms.create(600, 450, "island");
-  platforms.create(50, 250, "island");
-  platforms.create(650, 220, "island");
-  platforms.create(250, 520, "island");
-  platforms.create(250, 320, "island");
 
-  let stars = this.physics.add.group();
-  stars.create(22, 0, "star");
-  stars.create(122, 0, "star");
-  stars.create(222, 0, "star");
-  stars.create(322, 0, "star");
-  stars.create(422, 0, "star");
-  stars.create(522, 0, "star");
-  stars.create(622, 0, "star");
-  stars.create(722, 0, "star");
-
+  // this.player = this.game.add.sprite(0, 0, 'ship');
+  // this.player.anchor.setTo(0.5, 0.5);
+  // this.player.angle = -90; // Point the ship up
   
-  player = this.physics.add.sprite(380, 500, "player");
+  player = this.physics.add.sprite(0, 0, "player");
   player.setBounce(0.2);
   player.setCollideWorldBounds(true);
   this.physics.add.collider(player, platforms);
@@ -65,24 +49,24 @@ function create() {
     frameRate: 20
   })
   
-  this.anims.create({
-    key: 'left',
-    frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
-    frameRate: 10,
-    repeat: -1
-  })
+  // this.anims.create({
+  //   key: 'left',
+  //   frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
+  //   frameRate: 10,
+  //   repeat: -1
+  // })
   
-  this.anims.create({
-    key: 'right',
-    frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
-    frameRate: 10,
-    repeat: -1
-  })
+  // this.anims.create({
+  //   key: 'right',
+  //   frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
+  //   frameRate: 10,
+  //   repeat: -1
+  // })
 
   let score = 0
   let scoreText = this.add.text(16, 16, "Stars: 0", {
     fontSize: "32px",
-    fill: "#000",
+    fill: "#ffffff",
   })
 
   this.physics.add.overlap(
@@ -110,7 +94,7 @@ function update() {
   } else{
     player.setAccelerationY(160);
   }
-  
+
 }
 
 let player;
